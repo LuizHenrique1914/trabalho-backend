83a9a66c40896e2b9f01bdea2b4e9a21ed00bff8
Luiz Henrique Benicio Ribeiro <01649691@sempreuninassau.com.br>
2024-05-21 20:10:43 -0300

    Davi Bandeira Tavares - 01612958, Luiz Henrique- 01649691,evandro montesuma filho -01608339

---
+++ pages/api/hello.js
@@ -0,0 +1,85 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import openDB from "../../database/dbConfig";
+
+export default async function handler(req, res) {
+  const db = await openDB();
+
+  if (req.method === "GET") {
+    const id = req.query.id;
+    console.log("", id);
+
+    if (id === undefined) {
+      const usuarios = await db.all("SELECT * from Usuarios");
+
+      res.status(200).json(usuarios);
+    } else {
+      const usuario = await db.get("SELECT * from Usuarios WHERE id = ?", [id]);
+      res.status(200).json(usuario);
+    }
+  }
+
+  if (req.method === "POST") {
+    const new_usuario = req.body;
+
+    console.log("====================================");
+    console.log(new_usuario.nome, new_usuario.email);
+    console.log("====================================");
+
+    if (new_usuario.nome === undefined || new_usuario.nome === "") {
+      res.status(402).json({message: "nome é obrigatorio!"});
+    }
+
+    if (new_usuario.email === undefined || new_usuario.email === "") {
+      res.status(402).json({message: "email é obrigatorio!"});
+    }
+
+    const createUser = await db.prepare(
+      "INSERT INTO Usuarios (nome, email) VALUES (?, ?);"
+    );
+
+    const runCreat = await createUser.run(new_usuario.nome, new_usuario.email);
+
+    res.status(201).json({});
+  }
+
+  if (req.method === "PUT") {
+    const update_usuario = req.body;
+
+    const valid_usuario = await db.get("SELECT * from Usuarios WHERE id = ?", [
+      update_usuario.id,
+    ]);
+    if (valid_usuario === undefined) {
+      res.status(404).json({});
+    }
+
+    const updateUsuario = await db.prepare(
+      "UPDATE Usuarios SET nome = ?, email = ? WHERE id = ?"
+    );
+    const runCreat = await updateUsuario.run(
+      update_usuario.nome,
+      update_usuario.email,
+      update_usuario.id
+    );
+
+    res.status(200).json({});
+  }
+
+  if (req.method === "DELETE") {
+    const ID = req.body.id;
+
+    const valid_usuario = await db.get("SELECT * from Usuarios WHERE id = ?", [
+      ID,
+    ]);
+    if (valid_usuario === undefined) {
+      res.status(404).json({});
+    }
+
+    const deleteUsuario = await db.prepare(
+      "DELETE FROM Usuarios WHERE id = ?;"
+    );
+
+    const delete_Usuario = await deleteUsuario.run(ID);
+
+    res.status(201).json({});
+  }
+}
