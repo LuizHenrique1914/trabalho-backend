da0f7e60b3b824d25a1976396017b9655ce0170b
LuizHenrique1914 <luizdzf@gmail.com>
2024-05-25 00:53:58 +0000

    trablho definitivo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
-import database from '../../service/firebase';
+import {database} from '../../service/firebase';
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
